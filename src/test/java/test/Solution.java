1048f3e0e40598824401cb04e115d7feb58cbc08
boweny.yan <boweny.yan@qunar.com>
2020-02-07 15:08:49 +0800

    Add Question 20

--- src/test/java/test/Solution.java
+++ src/test/java/test/Solution.java
@@ -1,33 +1,21 @@
-package cn.yanbowen.lianbiao;
-
-import com.google.common.collect.Maps;
+package test;
 
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Stack;
 
 /**
  * Created by <a href=mailto:boweny.yan@qunar.com>boweny.yan</a>
- * DATE : 02/06/2020
- * TIME : 18:46
+ * DATE : 02/07/2020
+ * TIME : 14:59
  * PROJECT : DataStructureAndAlgorithm
- * PACKAGE : cn.yanbowen.lianbiao
+ * PACKAGE : test
  *
  * @author boweny.yan
- * <p>
- * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
- * <p>
- * 有效字符串需满足：
- * <p>
- * 左括号必须用相同类型的右括号闭合。
- * 左括号必须以正确的顺序闭合。
- * 注意空字符串可被认为是有效字符串。
  */
-public class _20_ValidParentheses {
-
+public class Solution {
     private HashMap<Character, Character> mappings;
 
-    public _20_ValidParentheses() {
+    public Solution() {
         this.mappings = new HashMap<Character, Character>();
         this.mappings.put(')', '(');
         this.mappings.put('}', '{');
