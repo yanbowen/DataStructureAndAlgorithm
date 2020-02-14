cf42c2a976cbd80cc472375f8e69a10bee3fa354
boweny.yan <boweny.yan@qunar.com>
2020-02-14 14:02:39 +0800

    Add Question 001

---
+++ src/main/java/cn/yanbowen/map/set/list/_1_TwoSum.java
@@ -0,0 +1,44 @@
+package cn.yanbowen.map.set.list;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by <a href=mailto:boweny.yan@qunar.com>boweny.yan</a>
+ * DATE : 02/12/2020
+ * TIME : 11:10
+ * PROJECT : DataStructureAndAlgorithm
+ * PACKAGE : cn.yanbowen.map.set.list
+ *
+ * @author boweny.yan
+ * <p>
+ * 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。
+ * <p>
+ * 示例:
+ * <p>
+ * 给定 nums = [2, 7, 11, 15], target = 9
+ * <p>
+ * 因为 nums[0] + nums[1] = 2 + 7 = 9
+ * 所以返回 [0, 1]
+ */
+public class _1_TwoSum {
+
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+
+            Map<Integer, Integer> map = new HashMap<>();
+            for (int i = 0; i < nums.length; i++) {
+                map.put(nums[i], i);
+            }
+
+            for (int i = 0; i < nums.length; i++) {
+                if (map.containsKey(target - nums[i]) && i != map.get(target - nums[i])) {
+                    return new int[]{i, map.get(target - nums[i])};
+                }
+            }
+            throw new RuntimeException();
+        }
+    }
+}
